f4bfdb58716a20a85e85f9562415306cad50de75
shbhmp <47439671+shbhmp@users.noreply.github.com>
2023-07-26 10:50:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,29 +1,24 @@
-//create a web server using express 
+//create a web server using express
+const express = require("express");
+const router = express.Router();
+//import the model
+const Comment = require("../models/comment");
 
-const express = require('express');
-const app = express();
-
-//create a get route for /comments
-app.get('/comments',(req,res)=>{
-    res.send('Get request to /comments');
+//get all comments
+router.get("/", (req, res) => {
+  Comment.find()
+    .then((comments) => res.json(comments))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
-//create a post route for /comments
-app.post('/comments',(req,res)=>{
-    res.send('Post request to /comments');
+//get a comment by id
+router.get("/:id", (req, res) => {
+  Comment.findById(req.params.id)
+    .then((comment) => res.json(comment))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
-//create a put route for /comments
-app.put('/comments',(req,res)=>{
-    res.send('Put request to /comments');
-});
-
-//create a delete route for /comments
-app.delete('/comments',(req,res)=>{
-    res.send('Delete request to /comments');
-});
-
-//listen on port 3000
-app.listen(3000,()=>{
-    console.log('Server started');
-});
+//add a comment
+router.post("/add", (req, res) => {
+  const newComment = new Comment({
+    text: req.body.text,
