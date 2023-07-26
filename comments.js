f1fa86be9da768ada8bc3235221c17a211a3cd83
shbhmp <47439671+shbhmp@users.noreply.github.com>
2023-07-26 10:59:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,5 +1,1 @@
-//create a web server using express
-const express = require("express");
-const router = express.Router();
-//import the model
-const Comment = require("../models/comment");
+// create web server
