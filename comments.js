47edcb8d1aa97056071cbb263d3d1559b907abed
shbhmp <47439671+shbhmp@users.noreply.github.com>
2023-07-26 10:52:21 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -3,22 +3,3 @@ const express = require("express");
 const router = express.Router();
 //import the model
 const Comment = require("../models/comment");
-
-//get all comments
-router.get("/", (req, res) => {
-  Comment.find()
-    .then((comments) => res.json(comments))
-    .catch((err) => res.status(400).json(`Error: ${err}`));
-});
-
-//get a comment by id
-router.get("/:id", (req, res) => {
-  Comment.findById(req.params.id)
-    .then((comment) => res.json(comment))
-    .catch((err) => res.status(400).json(`Error: ${err}`));
-});
-
-//add a comment
-router.post("/add", (req, res) => {
-  const newComment = new Comment({
-    text: req.body.text,
